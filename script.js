074bc9b0386c2e8e3ed52da028320cca2796bbc2
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-11-26 16:51:00 +0100

    add: sort by distance

--- script.js
+++ script.js
@@ -11,16 +11,9 @@ const adaCoordinates = {
 const apiKey = "e3f5b0642963ce590eb229aaedf81ce9299bd96c";
 const contractName = "nantes";
 
-const options = {
-	method: "GET",
-	headers: {
-		Accept: "application/json",
-	},
-};
-
 async function fetchApi() {
     const requestString = `https://api.jcdecaux.com/vls/v1/stations?contract=${contractName}&apiKey=${apiKey}&position=`;
-    const res = await fetch(requestString, options);
+    const res = await fetch(requestString);
     const data = await res.json();
 
     for (station in data) {
@@ -29,8 +22,9 @@ async function fetchApi() {
         console.log("🍐", data[station]);
     }
 
+    data.sort((a, b) => a.distance - b.distance);
+
     for (station in data) {
-        data.sort((a, b) => a.distance - b.distance);
         displayResults(data[station]);
     }
 
@@ -38,20 +32,22 @@ async function fetchApi() {
 fetchApi();
 
 
+// CALCUL DE LA DISTANCE // 
 function calculateDistance(lat, lng) {
-	var R = 6371; // Radius of the earth in km
-	var dLat = deg2rad(lat - adaCoordinates.lat); // deg2rad below
-	var dLng = deg2rad(lng - adaCoordinates.lng);
-	var a =
+	const R = 6371; // Radius of the earth in km 
+	const dLat = deg2rad(lat - adaCoordinates.lat); // deg2rad below
+	const dLng = deg2rad(lng - adaCoordinates.lng);
+	const a =
 		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 		Math.cos(deg2rad(adaCoordinates.lat)) *
 			Math.cos(deg2rad(lat)) *
 			Math.sin(dLng / 2) *
 			Math.sin(dLng / 2);
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	var d = R * c; // Distance in km
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	const d = R * c; // Distance in km
 	return d; // distance returned
 }
+
 function deg2rad(deg) {
 	return deg * (Math.PI / 180);
 }
